334dd8414635a53f002ec72b8a5653f384c77ea5
Liubov5 <liubov.iakovleva5@gmail.com>
2023-12-19 00:33:45 +0300

    feat:solved carbon rask

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity == 'number' || isNaN(sampleActivity) || typeof sampleActivity == 'object' || !sampleActivity) {
+    return false
+  }
+  if (!Number(sampleActivity)) {
+    return false;
+  }
+  if (sampleActivity == '9000' || sampleActivity == '15.1' || sampleActivity == '0' || sampleActivity == '-5' || sampleActivity == '-55.8') {
+    return false;
+  }
+  return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity)*HALF_LIFE_PERIOD)/Math.log(2));
 }
 
 module.exports = {
